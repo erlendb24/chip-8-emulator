c78962be19288c458b0c8522533e01276088decb
erlenebl <erlenebl@uio.no>
2026-07-15 21:06:48 +0200

    fixed keyboard layout mapping to correspond with chip-8 layout

--- chip_8.c
+++ chip_8.c
@@ -10,10 +10,10 @@
 #define MAX_ROW 32
 
 const uint8_t keyboard_lut[16] = {
-    KEY_ONE, KEY_TWO, KEY_THREE, KEY_FOUR,
-    KEY_Q, KEY_W, KEY_E, KEY_R,
-    KEY_A, KEY_S, KEY_D, KEY_F,
-    KEY_Z, KEY_X, KEY_C, KEY_V
+    KEY_X, KEY_ONE, KEY_TWO, KEY_THREE,
+    KEY_Q, KEY_W, KEY_E, KEY_A,
+    KEY_S, KEY_D, KEY_Z, KEY_C,
+    KEY_FOUR, KEY_R, KEY_F, KEY_V
 };
 
 uint8_t get_byte(uint16_t instruction) {
